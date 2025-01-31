8423bd55e10b095d72a8f22d01f61f2ca1db10d5
Zohaib Khalid <96901150+thezohaibkhalid@users.noreply.github.com>
2025-01-31 23:48:12 +0500

    Env uploading had some error that's why i am uploading it directly here

--- src/api/config.js
+++ src/api/config.js
@@ -1,9 +1,9 @@
 export const API_CONFIG = {
     BASE_URL: "https://api.openweathermap.org/data/2.5",
     GEO: "https://api.openweathermap.org/geo/1.0",
-    API_KEY: import.meta.env.VITE_OPENWEATHER_API_KEY,
+    API_KEY: '2d608f326d88af834ac6c9d6fa075635',
     DEFAULT_PARAMS: {
         units: "metric",
-        appid: import.meta.env.VITE_OPENWEATHER_API_KEY,
+        appid: '2d608f326d88af834ac6c9d6fa075635',
     },
 }
